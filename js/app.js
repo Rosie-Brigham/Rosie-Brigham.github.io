852d6015b6e5f468bcb749f8a4371e9666ec5c60
Rosie-Brigham <rosiebrigham@gmail.com>
2022-01-04 17:33:30 +0000

    finishes basic mobile view

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
+function toggle_class_style_visibility(className) {
+  const elements = document.getElementsByClassName(className);
+
+  for (i = 0; i < elements.length; i++) {
+    if(elements[i].style.display == "none" || elements[i].style.display == '')
+      elements[i].style.display = "block";
+    else
+      elements[i].style.display = "none";
+  }
+  
+}
+
+function toggle_visibility(id) {
+  const e = document.getElementById(id);
+  if(e.classList.contains('display-none'))
+    e.classList.remove("display-none");
+  else
+    e.classList.add("display-none");
+}
+
+function rotate_arrow(id) {
+
+}
+
 //  show - hide
 document.getElementById("hide").addEventListener("click", function(event){
-  console.log("hello")
   event.preventDefault()
   toggle_visibility("hide")
   toggle_visibility("show")
@@ -14,13 +37,7 @@ document.getElementById("show").addEventListener("click", function(event){
   toggle_visibility("menu")
 });
 
-function toggle_visibility(id) {
-  const e = document.getElementById(id);
-  if(e.classList.contains('display-none'))
-    e.classList.remove("display-none");
-  else
-    e.classList.add("display-none");
-}
+
 
 // Move info box on re-size
 var onresize = function() {
@@ -57,3 +74,41 @@ var desktopView = function() {
 // run mobile view once on page load
 mobileView()
 window.addEventListener("resize", onresize);
+
+
+
+
+
+
+
+// // mobile info expands
+document.getElementById("education").addEventListener("click", function(event){
+  event.preventDefault()
+  toggle_class_style_visibility("education-content")
+  document.getElementById("education").querySelector("#arrow").classList.toggle("rotated")
+});
+
+document.getElementById("career").addEventListener("click", function(event){
+  event.preventDefault()
+  toggle_class_style_visibility("career-content")
+  document.getElementById("career").querySelector("#arrow").classList.toggle("rotated")
+
+});
+
+document.getElementById("mm").addEventListener("click", function(event){
+  event.preventDefault()
+  toggle_class_style_visibility("mm-content")
+  document.getElementById("mm").querySelector("#arrow").classList.toggle("rotated")
+});
+
+document.getElementById("teaching").addEventListener("click", function(event){
+  event.preventDefault()
+  toggle_class_style_visibility("teaching-content")
+  document.getElementById("teaching").querySelector("#arrow").classList.toggle("rotated")
+});
+
+document.getElementById("contact").addEventListener("click", function(event){
+  event.preventDefault()
+  toggle_class_style_visibility("contact-content")
+  document.getElementById("contact").querySelector("#arrow").classList.toggle("rotated")
+});
--- js/canvas.js
+++ js/canvas.js
@@ -2,7 +2,6 @@
 
 const canvas = document.createElement("canvas")
 const sandbox = new GlslCanvas(canvas)
-console.log('yo')
 document.body.appendChild(canvas)
 
 sandbox.load(frag)
